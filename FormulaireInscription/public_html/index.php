71456c664b7c20d0fb158ee50ee15a31b9e592e8
Orion24 <nicolas.bertrand74@gmail.com>
2015-08-26 14:56:54 +0200

    Changement pour le formulaire (reprise du template)

---
+++ FormulaireInscription/public_html/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Fiche inscription utilisateur</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>Formulaire d'inscription</h1>
+        <form method="post" action="index.php" id="formInscription">
+            <label for="nom">Votre Nom</label> : <input type="text" name="nom" id="nom" maxlength="20" required /> <br />
+            <label for="prenom">Votre Prenom</label> : <input type="text" name="prenom" id="nom" maxlength="20" required /> <br />
+            <label for="pseudo">Votre Pseudo</label> : <input type="text" name="pseudo" id="pseudo" maxlength="20" required /> <br />
+            <label for="pass">Votre mot de passe :</label><input type="password" name="pass" id="pass" required /> <br />
+            <label for="description">Mini description :</label><textarea name="description" id="description" maxlength="100" required ></textarea> <br />
+            <label for="email">Votre E-mail</label> : <input type="email" name="email" id="email" required /> <br />
+            <label for="date">Votre Date de naissance</label> : <input type="date" name="date" id="date" required /><br />
+        </form>
+    </body>
+</html>
