41264d8497a3503251f47dc708a0fc2ad21de613
Orion24 <nicolas.bertrand74@gmail.com>
2015-09-23 13:56:09 +0200

    Affichage des détails selon l'utilisateur

--- FormulaireInscription/public_html/AffichageNom.php
+++ FormulaireInscription/public_html/AffichageNom.php
@@ -6,20 +6,44 @@
         $html .= '<table style="border-collapse: collapse;border:1px solid black;">';
         $html .= "<th>Nom</th>";
         $html .= "<th>Prenom</th>";
-        foreach  (getUser() as $row) 
+        $html .= "<th>Détail</th>";
+        foreach  (getListUsers() as $row) 
         {
             $html .= "<tr>";               
             $html .= "<td>".$row['nom']."</td>";
             $html .= "<td>".$row['prenom']."</td>";
+            $html .= "<td><a href=\"http://127.0.0.1/siteInscription/AffichageNom.php?value=".$row['idUtilisateur']."\">détail</a>";
             $html .= "</tr>";
         }
-        $html .= "</table";
+        $html .= "</table>";
         return $html;
     }
+    function getListUsers()
+    {
+       $query = 'SELECT nom, prenom, idUtilisateur FROM utilisateurs';
+       return getDb()->query($query);
+    }
+    
     function getUser()
     {
-       $query = 'SELECT nom, prenom FROM utilisateurs';
-       return getDb()->query($query);
+        if(isset($_REQUEST['value']) && is_numeric($_REQUEST['value'])) 
+        {
+            $query = 'SELECT nom, prenom, description, email, dateNaissance FROM utilisateurs WHERE idUtilisateur='.$_REQUEST['value'];
+            $answer = getDb()->query($query);//execute the query
+            $tabUser = $answer->fetch(PDO::FETCH_ASSOC);//We make the answer an associotive array
+            $html = "";
+            $html .= '<table style="border-collapse: collapse;border:1px solid black;">';
+            $html .= "<th>Nom</th><th>Prenom</th><th>Description</th><th>Email</th><th>Date de naissance</th>";
+            $html .= "<tr>";
+            $html .= "<td>".$tabUser['nom']."</td>";
+            $html .= "<td>".$tabUser['prenom']."</td>";
+            $html .= "<td>".$tabUser['description']."</td>";
+            $html .= "<td>".$tabUser['email']."</td>";
+            $html .= "<td>".$tabUser['dateNaissance']."</td>";
+            $html .= "</tr>";
+            $html .= "</table>";
+            return $html;
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -36,6 +60,10 @@ and open the template in the editor.
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <?php echo getArrayUser() ?>
+        <?php 
+            echo getArrayUser();
+            echo getUser();
+        ?>
+        
     </body>
 </html>
