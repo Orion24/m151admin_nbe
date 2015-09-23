3e72072cd29ca5d9a221ea943dd296848358e9e6
Orion24 <nicolas.bertrand74@gmail.com>
2015-09-23 14:50:20 +0200

    Affichage des valeurs de l'utilisateur que l'on a choisi dans la liste d'utilisateur dans le formulaire

--- FormulaireInscription/public_html/index.php
+++ FormulaireInscription/public_html/index.php
@@ -1,5 +1,13 @@
 <?php
     include 'functionDb.php';
+    $nom = "";
+    $prenom = "";
+    $description = "";
+    $pseudo = "";
+    $email = "";
+    $dateDeNaissance = "";
+    $modification = false;
+    
     function testArg($tab) //Check if there are empty cells
     {
         foreach($tab as $value)
@@ -39,6 +47,25 @@
         insertUser();
         header('Location: AffichageNom.php');
     }
+    
+    function getInfoUser($idUser)
+    {
+        $query = 'SELECT nom, prenom, description, pseudo, email, dateNaissance FROM utilisateurs WHERE idUtilisateur='.$idUser;
+        $answer = getDb()->query($query);//execute the query
+        return $answer->fetch(PDO::FETCH_ASSOC);//We make the answer an associotive array
+    }
+    
+    if(isset($_REQUEST['value']) && is_numeric($_REQUEST['value']))
+    {
+        $modification = true;
+        $tabInfoUser = getInfoUser($_REQUEST['value']);
+        $nom = $tabInfoUser['nom'];
+        $prenom = $tabInfoUser['prenom'];
+        $pseudo = $tabInfoUser['pseudo'];
+        $description = $tabInfoUser['description'];
+        $email = $tabInfoUser['email'];
+        $dateDeNaissance = $tabInfoUser['dateNaissance'];
+    }
 ?>
 <!DOCTYPE html>
 <!--
@@ -57,14 +84,14 @@ and open the template in the editor.
     <body>
         <h1>Formulaire d'inscription</h1>
         <form method="post" action="index.php" id="formInscription">
-            <label for="nom">Votre Nom</label> : <input type="text" name="nom" id="nom" maxlength="50" required /> <br />
-            <label for="prenom">Votre Prenom</label> : <input type="text" name="prenom" id="nom" maxlength="30" required /> <br />
-            <label for="pseudo">Votre Pseudo</label> : <input type="text" name="pseudo" id="pseudo" maxlength="20" required /> <br />
+            <label for="nom">Votre Nom</label> : <input type="text" name="nom" id="nom" maxlength="50" required value="<?= $nom ?>"/> <br />
+            <label for="prenom">Votre Prenom</label> : <input type="text" name="prenom" id="nom" maxlength="30" value="<?= $prenom ?>" required /> <br />
+            <label for="pseudo">Votre Pseudo</label> : <input type="text" name="pseudo" id="pseudo" maxlength="20" value="<?= $pseudo ?>" required /> <br />
             <label for="pass">Votre mot de passe :</label><input type="password" name="pass" id="pass" required /> <br />
             <label for="passconf">Confirmer mot de passe :</label><input type="password" name="passconf" id="passconf" required /> <br />
-            <label for="description">Mini description :</label><textarea name="description" id="description" maxlength="100" ></textarea> <br />
-            <label for="email">Votre E-mail</label> : <input type="email" name="email" id="email" maxlength="200" required /> <br />
-            <label for="date">Votre Date de naissance</label> : <input type="date" name="date" id="date" required /><br />
+            <label for="description">Mini description :</label><textarea name="description" id="description" maxlength="100" value="<?= $description ?>" ></textarea> <br />
+            <label for="email">Votre E-mail</label> : <input type="email" name="email" id="email" maxlength="200" value="<?= $email ?>" required /> <br />
+            <label for="date">Votre Date de naissance</label> : <input type="date" name="date" id="date" value="<?= $dateDeNaissance ?>" required /><br />
             <input type="submit" value="Envoyer" name="boutonEnvoyer"/>
         </form>
         
