9a230d84cc9295a42efda551d05b3c5eda3b9cf2
Orion24 <nicolas.bertrand74@gmail.com>
2015-09-30 15:47:12 +0200

    Précision du type que le veut pour le filtrage de la préparation de la requête

--- FormulaireInscription/public_html/functionDb.php
+++ FormulaireInscription/public_html/functionDb.php
@@ -24,26 +24,26 @@ function getDb()
 function insertUser($lastname, $firstname, $pseudo, $pass, $description, $email, $date)
     {
         $request = getDb()->prepare("INSERT INTO `m151admin_nbe`.`utilisateurs` (`idUtilisateur`, `nom`, `prenom`, `pseudo`, `motDePasse`, `description`, `email`, `dateNaissance`) VALUES (NULL, :lastname, :firstname, :pseudo, SHA1(:pass), :description, :email, :date)");
-        $request->bindParam(':lastname', $lastname);
-        $request->bindParam(':firstname', $firstname);
-        $request->bindParam(':pseudo', $pseudo);
-        $request->bindParam(':pass', $pass);
-        $request->bindParam(':description', $description);
-        $request->bindParam(':email', $email);
-        $request->bindParam(':date', $date);
+        $request->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+        $request->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+        $request->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+        $request->bindParam(':pass', $pass, PDO::PARAM_STR);
+        $request->bindParam(':description', $description, PDO::PARAM_STR);
+        $request->bindParam(':email', $email, PDO::PARAM_STR);
+        $request->bindParam(':date', $date, PDO::PARAM_STR);
         $request->execute(); 
     }
     
 function modifyUser($lastname, $firstname, $pseudo, $pass, $description, $email, $date, $idUser)
     {
         $request = getDb()->prepare("UPDATE `m151admin_nbe`.`utilisateurs` SET `nom` = :lastname, `prenom` = :firstname,  `pseudo` = :pseudo, `motDePasse` = SHA1(:pass), `description` = :description, `email` = :email, `dateNaissance` = :date WHERE `utilisateurs`.`idUtilisateur` = ".$idUser.";");
-        $request->bindParam(':lastname', $lastname);
-        $request->bindParam(':firstname', $firstname);
-        $request->bindParam(':pseudo', $pseudo);
-        $request->bindParam(':pass', $pass);
-        $request->bindParam(':description', $description);
-        $request->bindParam(':email', $email);
-        $request->bindParam(':date', $date);
+        $request->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+        $request->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+        $request->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+        $request->bindParam(':pass', $pass, PDO::PARAM_STR);
+        $request->bindParam(':description', $description, PDO::PARAM_STR);
+        $request->bindParam(':email', $email, PDO::PARAM_STR);
+        $request->bindParam(':date', $date, PDO::PARAM_STR);
         $request->execute();
     }
 function getInfoUser($idUser)
