b480f0b92b0790f84919eda26b5f4b1382fe438d
Orion24 <nicolas.bertrand74@gmail.com>
2015-10-28 12:45:16 +0100

    Déplacement des fonctions en rapport avec la base de données dans functionDb.php

--- FormulaireInscription/public_html/AffichageNom.php
+++ FormulaireInscription/public_html/AffichageNom.php
@@ -1,6 +1,5 @@
 <?php
     session_start();
-    //include_once 'functionDb.php';
     if(empty($_SESSION['nom']))
     {
         session_write_close(); // to be sure
@@ -40,42 +39,7 @@
         $html .= "</table>";
         return $html;
     }
-    function getListUsers()
-    {
-       $query = 'SELECT nom, prenom, pseudo, idUtilisateur FROM utilisateurs';
-       $answer = getDb()->query($query); //execute the query
-       return $answer->fetchAll(PDO::FETCH_ASSOC); //We make the answer an associotive array
-    }
-    
-    function getUser()
-    {
-        if(isset($_REQUEST['value']) && is_numeric($_REQUEST['value'])) 
-        {
-            $query = 'SELECT nom, prenom, pseudo, description, email, dateNaissance FROM utilisateurs WHERE idUtilisateur='.$_REQUEST['value'];
-            $answer = getDb()->query($query);//execute the query
-            $tabUser = $answer->fetch(PDO::FETCH_ASSOC);//We make the answer an associotive array
-            $html = "";
-            if($tabUser != null)//if the user exist
-            {
-                $html .= '<table style="border-collapse: collapse;border:1px solid black;">';
-                $html .= "<th>Nom</th><th>Prenom</th><th>Pseudo</th><th>Description</th><th>Email</th><th>Date de naissance</th>";
-                $html .= "<tr><td>".$tabUser['nom']."</td>";
-                $html .= "<td>".$tabUser['prenom']."</td>";
-                $html .= "<td>".$tabUser['pseudo']."</td>";
-                $html .= "<td>".$tabUser['description']."</td>";
-                $html .= "<td>".$tabUser['email']."</td>";
-                $html .= "<td>".$tabUser['dateNaissance']."</td></tr></table>";
-            }
-            return $html;
-        }
-    }
-    
-    function deleteUser($idUser)
-    {
-        $request = getDb()->prepare("DELETE FROM `m151admin_nbe`.`utilisateurs` WHERE `utilisateurs`.`idUtilisateur` = :idUser");
-        $request->bindParam(':idUser', $idUser);
-        $request->execute();
-    }
+
     if(isset($_REQUEST['delete']) && is_numeric($_REQUEST['delete']))
     {
         deleteUser($_REQUEST['delete']);
--- FormulaireInscription/public_html/functionDb.php
+++ FormulaireInscription/public_html/functionDb.php
@@ -57,4 +57,39 @@ function login($username, $pass) {
     return $request->fetch(PDO::FETCH_ASSOC);
 }
 
-
+function getListUsers()
+    {
+       $query = 'SELECT nom, prenom, pseudo, idUtilisateur FROM utilisateurs';
+       $answer = getDb()->query($query); //execute the query
+       return $answer->fetchAll(PDO::FETCH_ASSOC); //We make the answer an associotive array
+    }
+    
+    function getUser()
+    {
+        if(isset($_REQUEST['value']) && is_numeric($_REQUEST['value'])) 
+        {
+            $query = 'SELECT nom, prenom, pseudo, description, email, dateNaissance FROM utilisateurs WHERE idUtilisateur='.$_REQUEST['value'];
+            $answer = getDb()->query($query);//execute the query
+            $tabUser = $answer->fetch(PDO::FETCH_ASSOC);//We make the answer an associotive array
+            $html = "";
+            if($tabUser != null)//if the user exist
+            {
+                $html .= '<table style="border-collapse: collapse;border:1px solid black;">';
+                $html .= "<th>Nom</th><th>Prenom</th><th>Pseudo</th><th>Description</th><th>Email</th><th>Date de naissance</th>";
+                $html .= "<tr><td>".$tabUser['nom']."</td>";
+                $html .= "<td>".$tabUser['prenom']."</td>";
+                $html .= "<td>".$tabUser['pseudo']."</td>";
+                $html .= "<td>".$tabUser['description']."</td>";
+                $html .= "<td>".$tabUser['email']."</td>";
+                $html .= "<td>".$tabUser['dateNaissance']."</td></tr></table>";
+            }
+            return $html;
+        }
+    }
+    
+    function deleteUser($idUser)
+    {
+        $request = getDb()->prepare("DELETE FROM `m151admin_nbe`.`utilisateurs` WHERE `utilisateurs`.`idUtilisateur` = :idUser");
+        $request->bindParam(':idUser', $idUser);
+        $request->execute();
+    }
